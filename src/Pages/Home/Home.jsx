d67fac1cdd121f673714e8c568c0f0df8f7cc6b2
dmitrmedv <106028486+dmitrmedv@users.noreply.github.com>
2024-12-10 17:33:52 +0200

    formik

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,5 +1,26 @@
+import { Field, Form, Formik } from "formik";
+
 const Home = () => {
-  return <div>Home</div>;
+  let obj = null;
+  return (
+    <>
+      <Formik
+        initialValues={obj ?? { name: "", age: "" }}
+        onSubmit={(values) => console.log(values)}
+      >
+        {() => (
+          <Form>
+            <Field type="text" name="name" />
+            <Field type="text" name="age" />
+            <button type="submit">Submit</button>
+          </Form>
+        )}
+      </Formik>
+      <button type="button" onClick={() => {}}>
+        CLICK
+      </button>
+    </>
+  );
 };
 
 export default Home;
